d732592c35c3976effa695e20fa8a44d72b7b0b5
winter100 <100taeng@gmail.com>
2026-07-11 15:19:16 +0900

    feat: 프리즈마 스키마 추가

--- prisma/migrations/0_init/migration.sql
+++ prisma/migrations/0_init/migration.sql
@@ -10,6 +10,9 @@ CREATE TYPE "public"."StatType" AS ENUM ('ENTRY', 'LIMIT');
 -- CreateEnum
 CREATE TYPE "public"."UserRole" AS ENUM ('USER', 'ADMIN');
 
+-- CreateEnum
+CREATE TYPE "public"."AffixType" AS ENUM ('PREFIX', 'SUFFIX', 'INFUSION', 'PARTHOLN');
+
 -- CreateTable
 CREATE TABLE "public"."items" (
     "id" SERIAL NOT NULL,
@@ -17,9 +20,28 @@ CREATE TABLE "public"."items" (
     "description" TEXT,
     "categoryId" INTEGER NOT NULL,
     "tierId" INTEGER NOT NULL,
-    "image" TEXT NOT NULL,
+    "image" TEXT,
 
     CONSTRAINT "items_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."equipment_enhancements" (
+    "id" SERIAL NOT NULL,
+    "itemId" INTEGER NOT NULL,
+    "stepName" TEXT NOT NULL,
+
+    CONSTRAINT "equipment_enhancements_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."items_stats" (
+    "id" SERIAL NOT NULL,
+    "statId" INTEGER NOT NULL,
+    "value" INTEGER NOT NULL,
+    "equipmentStepId" INTEGER NOT NULL,
+
+    CONSTRAINT "items_stats_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -28,16 +50,6 @@ CREATE TABLE "public"."categories" (
     "name" TEXT NOT NULL,
 
     CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "public"."recipes" (
-    "id" SERIAL NOT NULL,
-    "resultId" INTEGER NOT NULL,
-    "materialId" INTEGER NOT NULL,
-    "quantity" INTEGER NOT NULL,
-
-    CONSTRAINT "recipes_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -69,6 +81,27 @@ CREATE TABLE "public"."enchants" (
 );
 
 -- CreateTable
+CREATE TABLE "public"."infusions" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "rankId" INTEGER NOT NULL,
+    "affixId" INTEGER NOT NULL,
+    "category" "public"."EnchantCategory" NOT NULL DEFAULT 'INFUSION',
+
+    CONSTRAINT "infusions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."partholns" (
+    "id" SERIAL NOT NULL,
+    "name" INTEGER NOT NULL,
+    "rank" INTEGER NOT NULL,
+    "affixId" INTEGER NOT NULL,
+
+    CONSTRAINT "partholns_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "public"."ranks" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
@@ -80,7 +113,7 @@ CREATE TABLE "public"."ranks" (
 CREATE TABLE "public"."affixs" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
-    "value" TEXT NOT NULL,
+    "value" "public"."AffixType" NOT NULL,
 
     CONSTRAINT "affixs_pkey" PRIMARY KEY ("id")
 );
@@ -89,6 +122,7 @@ CREATE TABLE "public"."affixs" (
 CREATE TABLE "public"."stats" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
+    "image" TEXT,
 
     CONSTRAINT "stats_pkey" PRIMARY KEY ("id")
 );
@@ -96,9 +130,11 @@ CREATE TABLE "public"."stats" (
 -- CreateTable
 CREATE TABLE "public"."effects" (
     "id" SERIAL NOT NULL,
-    "enchantId" INTEGER NOT NULL,
+    "enchantId" INTEGER,
     "statId" INTEGER NOT NULL,
     "value" TEXT NOT NULL,
+    "partholnId" INTEGER,
+    "infusionId" INTEGER,
 
     CONSTRAINT "effects_pkey" PRIMARY KEY ("id")
 );
@@ -116,7 +152,8 @@ CREATE TABLE "public"."slots" (
 CREATE TABLE "public"."enchantSlots" (
     "id" SERIAL NOT NULL,
     "slotId" INTEGER NOT NULL,
-    "enchantId" INTEGER NOT NULL,
+    "enchantId" INTEGER,
+    "infusionId" INTEGER,
 
     CONSTRAINT "enchantSlots_pkey" PRIMARY KEY ("id")
 );
@@ -176,9 +213,10 @@ CREATE TABLE "public"."itemDrops" (
 -- CreateTable
 CREATE TABLE "public"."enchantDrops" (
     "id" SERIAL NOT NULL,
-    "enchantId" INTEGER NOT NULL,
+    "enchantId" INTEGER,
     "raidId" INTEGER,
     "itemId" INTEGER,
+    "infusionId" INTEGER,
 
     CONSTRAINT "enchantDrops_pkey" PRIMARY KEY ("id")
 );
@@ -252,8 +290,122 @@ CREATE TABLE "public"."refreshToken" (
     CONSTRAINT "refreshToken_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "public"."grinds" (
+    "id" SERIAL NOT NULL,
+    "titleId" INTEGER NOT NULL,
+    "statId" INTEGER NOT NULL,
+    "stat_one_value" INTEGER NOT NULL,
+    "stat_max_value" INTEGER NOT NULL,
+
+    CONSTRAINT "grinds_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."grinds_slots" (
+    "id" SERIAL NOT NULL,
+    "grindId" INTEGER NOT NULL,
+    "slotId" INTEGER NOT NULL,
+
+    CONSTRAINT "grinds_slots_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."grinds_ingredients" (
+    "id" SERIAL NOT NULL,
+    "grindId" INTEGER NOT NULL,
+    "itemId" INTEGER NOT NULL,
+    "quantity" INTEGER NOT NULL,
+
+    CONSTRAINT "grinds_ingredients_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."item_sets" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "item_sets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."items_sets_effects" (
+    "id" SERIAL NOT NULL,
+    "levelId" INTEGER NOT NULL,
+    "setId" INTEGER NOT NULL,
+    "statId" INTEGER NOT NULL,
+    "statValue" INTEGER NOT NULL,
+
+    CONSTRAINT "items_sets_effects_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."items_sets_level" (
+    "id" SERIAL NOT NULL,
+    "level" INTEGER NOT NULL,
+
+    CONSTRAINT "items_sets_level_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."items_sets_name_list" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "items_sets_name_list_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."items_sets_slots_list" (
+    "id" SERIAL NOT NULL,
+    "setId" INTEGER NOT NULL,
+    "slotId" INTEGER NOT NULL,
+
+    CONSTRAINT "items_sets_slots_list_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."items_sets_list" (
+    "id" SERIAL NOT NULL,
+    "setId" INTEGER NOT NULL,
+    "itemSetListId" INTEGER NOT NULL,
+
+    CONSTRAINT "items_sets_list_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."recipes" (
+    "id" SERIAL NOT NULL,
+    "resultId" INTEGER NOT NULL,
+    "materialId" INTEGER NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "description" TEXT,
+
+    CONSTRAINT "recipes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."_ItemToSlot" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL,
+
+    CONSTRAINT "_ItemToSlot_AB_pkey" PRIMARY KEY ("A","B")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "items_name_key" ON "public"."items"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "equipment_enhancements_itemId_stepName_key" ON "public"."equipment_enhancements"("itemId", "stepName");
+
+-- CreateIndex
+CREATE INDEX "items_stats_equipmentStepId_idx" ON "public"."items_stats"("equipmentStepId");
+
+-- CreateIndex
+CREATE INDEX "items_stats_statId_idx" ON "public"."items_stats"("statId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "items_stats_equipmentStepId_statId_key" ON "public"."items_stats"("equipmentStepId", "statId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "categories_name_key" ON "public"."categories"("name");
@@ -272,9 +424,6 @@ CREATE UNIQUE INDEX "ranks_name_key" ON "public"."ranks"("name");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "affixs_name_key" ON "public"."affixs"("name");
-
--- CreateIndex
-CREATE UNIQUE INDEX "affixs_value_key" ON "public"."affixs"("value");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "stats_name_key" ON "public"."stats"("name");
@@ -312,6 +461,30 @@ CREATE UNIQUE INDEX "refreshToken_token_key" ON "public"."refreshToken"("token")
 -- CreateIndex
 CREATE UNIQUE INDEX "refreshToken_userId_key" ON "public"."refreshToken"("userId");
 
+-- CreateIndex
+CREATE INDEX "grinds_ingredients_itemId_idx" ON "public"."grinds_ingredients"("itemId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "grinds_ingredients_grindId_itemId_key" ON "public"."grinds_ingredients"("grindId", "itemId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "item_sets_name_key" ON "public"."item_sets"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "items_sets_name_list_name_key" ON "public"."items_sets_name_list"("name");
+
+-- CreateIndex
+CREATE INDEX "recipes_materialId_idx" ON "public"."recipes"("materialId");
+
+-- CreateIndex
+CREATE INDEX "recipes_resultId_idx" ON "public"."recipes"("resultId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "recipes_resultId_materialId_key" ON "public"."recipes"("resultId", "materialId");
+
+-- CreateIndex
+CREATE INDEX "_ItemToSlot_B_index" ON "public"."_ItemToSlot"("B");
+
 -- AddForeignKey
 ALTER TABLE "public"."items" ADD CONSTRAINT "items_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "public"."categories"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
 
@@ -319,10 +492,13 @@ ALTER TABLE "public"."items" ADD CONSTRAINT "items_categoryId_fkey" FOREIGN KEY 
 ALTER TABLE "public"."items" ADD CONSTRAINT "items_tierId_fkey" FOREIGN KEY ("tierId") REFERENCES "public"."tiers"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE "public"."recipes" ADD CONSTRAINT "recipes_materialId_fkey" FOREIGN KEY ("materialId") REFERENCES "public"."items"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "public"."equipment_enhancements" ADD CONSTRAINT "equipment_enhancements_itemId_fkey" FOREIGN KEY ("itemId") REFERENCES "public"."items"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "public"."recipes" ADD CONSTRAINT "recipes_resultId_fkey" FOREIGN KEY ("resultId") REFERENCES "public"."items"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "public"."items_stats" ADD CONSTRAINT "items_stats_equipmentStepId_fkey" FOREIGN KEY ("equipmentStepId") REFERENCES "public"."equipment_enhancements"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."items_stats" ADD CONSTRAINT "items_stats_statId_fkey" FOREIGN KEY ("statId") REFERENCES "public"."stats"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "public"."enchants" ADD CONSTRAINT "enchants_affixId_fkey" FOREIGN KEY ("affixId") REFERENCES "public"."affixs"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
@@ -334,13 +510,31 @@ ALTER TABLE "public"."enchants" ADD CONSTRAINT "enchants_rankId_fkey" FOREIGN KE
 ALTER TABLE "public"."enchants" ADD CONSTRAINT "enchants_tierId_fkey" FOREIGN KEY ("tierId") REFERENCES "public"."tiers"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- AddForeignKey
+ALTER TABLE "public"."infusions" ADD CONSTRAINT "infusions_affixId_fkey" FOREIGN KEY ("affixId") REFERENCES "public"."affixs"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "public"."infusions" ADD CONSTRAINT "infusions_rankId_fkey" FOREIGN KEY ("rankId") REFERENCES "public"."ranks"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "public"."partholns" ADD CONSTRAINT "partholns_affixId_fkey" FOREIGN KEY ("affixId") REFERENCES "public"."affixs"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
 ALTER TABLE "public"."effects" ADD CONSTRAINT "effects_enchantId_fkey" FOREIGN KEY ("enchantId") REFERENCES "public"."enchants"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."effects" ADD CONSTRAINT "effects_infusionId_fkey" FOREIGN KEY ("infusionId") REFERENCES "public"."infusions"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."effects" ADD CONSTRAINT "effects_partholnId_fkey" FOREIGN KEY ("partholnId") REFERENCES "public"."partholns"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "public"."effects" ADD CONSTRAINT "effects_statId_fkey" FOREIGN KEY ("statId") REFERENCES "public"."stats"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "public"."enchantSlots" ADD CONSTRAINT "enchantSlots_enchantId_fkey" FOREIGN KEY ("enchantId") REFERENCES "public"."enchants"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."enchantSlots" ADD CONSTRAINT "enchantSlots_infusionId_fkey" FOREIGN KEY ("infusionId") REFERENCES "public"."infusions"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "public"."enchantSlots" ADD CONSTRAINT "enchantSlots_slotId_fkey" FOREIGN KEY ("slotId") REFERENCES "public"."slots"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
@@ -367,6 +561,9 @@ ALTER TABLE "public"."itemDrops" ADD CONSTRAINT "itemDrops_raidId_fkey" FOREIGN 
 ALTER TABLE "public"."enchantDrops" ADD CONSTRAINT "enchantDrops_enchantId_fkey" FOREIGN KEY ("enchantId") REFERENCES "public"."enchants"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "public"."enchantDrops" ADD CONSTRAINT "enchantDrops_infusionId_fkey" FOREIGN KEY ("infusionId") REFERENCES "public"."infusions"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "public"."enchantDrops" ADD CONSTRAINT "enchantDrops_itemId_fkey" FOREIGN KEY ("itemId") REFERENCES "public"."items"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
@@ -386,4 +583,55 @@ ALTER TABLE "public"."characters_skills" ADD CONSTRAINT "characters_skills_skill
 
 -- AddForeignKey
 ALTER TABLE "public"."refreshToken" ADD CONSTRAINT "refreshToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."grinds" ADD CONSTRAINT "grinds_statId_fkey" FOREIGN KEY ("statId") REFERENCES "public"."stats"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."grinds" ADD CONSTRAINT "grinds_titleId_fkey" FOREIGN KEY ("titleId") REFERENCES "public"."raidTitles"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."grinds_slots" ADD CONSTRAINT "grinds_slots_grindId_fkey" FOREIGN KEY ("grindId") REFERENCES "public"."grinds"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."grinds_slots" ADD CONSTRAINT "grinds_slots_slotId_fkey" FOREIGN KEY ("slotId") REFERENCES "public"."slots"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."grinds_ingredients" ADD CONSTRAINT "grinds_ingredients_grindId_fkey" FOREIGN KEY ("grindId") REFERENCES "public"."grinds"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."grinds_ingredients" ADD CONSTRAINT "grinds_ingredients_itemId_fkey" FOREIGN KEY ("itemId") REFERENCES "public"."items"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."items_sets_effects" ADD CONSTRAINT "items_sets_effects_levelId_fkey" FOREIGN KEY ("levelId") REFERENCES "public"."items_sets_level"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."items_sets_effects" ADD CONSTRAINT "items_sets_effects_setId_fkey" FOREIGN KEY ("setId") REFERENCES "public"."item_sets"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."items_sets_effects" ADD CONSTRAINT "items_sets_effects_statId_fkey" FOREIGN KEY ("statId") REFERENCES "public"."stats"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."items_sets_slots_list" ADD CONSTRAINT "items_sets_slots_list_setId_fkey" FOREIGN KEY ("setId") REFERENCES "public"."item_sets"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."items_sets_slots_list" ADD CONSTRAINT "items_sets_slots_list_slotId_fkey" FOREIGN KEY ("slotId") REFERENCES "public"."slots"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."items_sets_list" ADD CONSTRAINT "items_sets_list_itemSetListId_fkey" FOREIGN KEY ("itemSetListId") REFERENCES "public"."items_sets_name_list"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."items_sets_list" ADD CONSTRAINT "items_sets_list_setId_fkey" FOREIGN KEY ("setId") REFERENCES "public"."item_sets"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."recipes" ADD CONSTRAINT "recipes_materialId_fkey" FOREIGN KEY ("materialId") REFERENCES "public"."items"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."recipes" ADD CONSTRAINT "recipes_resultId_fkey" FOREIGN KEY ("resultId") REFERENCES "public"."items"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."_ItemToSlot" ADD CONSTRAINT "_ItemToSlot_A_fkey" FOREIGN KEY ("A") REFERENCES "public"."items"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."_ItemToSlot" ADD CONSTRAINT "_ItemToSlot_B_fkey" FOREIGN KEY ("B") REFERENCES "public"."slots"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
--- prisma/migrations/20260513045612_change_affix_value_to_enum/migration.sql
+++
@@ -1,8 +0,0 @@
-CREATE TYPE "public"."AffixType" AS ENUM ('PREFIX', 'SUFFIX', 'INFUSION', 'PARTHOLN');
-
-DROP INDEX "public"."affixs_value_key";
-
-
-ALTER TABLE "public"."affixs" ALTER COLUMN "value" TYPE "public"."AffixType" USING ("value"::text::"public"."AffixType");
-
-CREATE UNIQUE INDEX "affixs_value_key" ON "public"."affixs"("value");
